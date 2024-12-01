932cef3a90017d6d838ddab06d2dd2be040f46c7
aramovsepyan2006@outlook.com <aramovsepyan2006@outlook.com>
2024-12-01 19:10:57 +0500

    Task5

---
+++ Tyuiu.OvsepyanAA.Sprint5.Task5.V10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+
+using Tyuiu.OvsepyanAA.Sprint5.Task5.V10.Lib;
+
+namespace Tyuiu.OvsepyanAA.Sprint5.Task5.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("*****************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                  *");
+            Console.WriteLine("*****************************************************************************************************");
+
+            string path = @"D:\DataSprint5\InPutDataFileTask5V10.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("*****************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                        *");
+            Console.WriteLine("*****************************************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine("Сумма четных элементов файла = " + res);
+            Console.ReadKey();
+        }
+    }
+}
